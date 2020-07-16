a056e8600272252b75b807464e9be200cd80eb57
JRLoveridge <48652336+JRLoveridge@users.noreply.github.com>
2020-07-16 18:59:51 -0500

    Update and rename leo_modelling.py to main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from modelling import *
+
+if __name__ == '__main__':
+
+    #comment out if no MPI
+    import mpi4py.MPI as MPI
+    comm = MPI.COMM_WORLD
+
+    Terra = Satellite.Terra()
+
+    #wrap one of the model functions with any necessary parameters.
+    #kwargs will be passed from the satellite for all of the lat/lon/time/solar_zenith/viewing_zenith etc
+    #information that may be necessary to evaluate the model.
+    def model(**kwargs):
+        return linear_in_vza(gradient=1.0, **kwargs)
+
+    grid = Grid(np.linspace(-90, 90, 1801), np.linspace(-180.0,180.0, 3601),
+                                           save_period=1.0)
+
+    #The pre-made MISR/MODIS have extremely high temporal sampling to simulate high resolution measurements
+    #as such they are quite intensive in both memory and processing. Such a large number of simulated pixels
+    #is only necessary for the highest of resolution climate marble grids (0.05 degrees)
+    #instruments = Instrument(330.0, 1.0/50000, 100, model=model, descending_only=True)
+    instruments=[Instrument.MODIS(model=model, descending_only=True)]
+    Terra.add_instruments(instruments)
+    driver(0.0, 16.0, grid, Terra, '/data/keeling/a/jesserl2/c/MODIS_16day_linear_in_time', mpi_comm=comm) #set to None if no mpi
